5a07bec0a04a768bb81d738952fc503be5e23f5c
vandole2121 <111689415+vandole2121@users.noreply.github.com>
2023-05-16 13:27:15 +0200

    Update cred.sh

--- cred.sh
+++ cred.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 ### Credentials
 
-echo "Setup credentials... (Ctrl-C to quit, Enter to continue)"
+echo "Setup credentials... Make sure you are on the right user! (Ctrl-C to quit, Enter to continue)"
 read
 REALNAME=""
 EMAIL=""
@@ -9,7 +11,7 @@ read REALNAME
 echo "Enter EMAIL:"
 read EMAIL
 
-passphrase2pgp -e -s -a > prv.asc
+REALNAME=$REALNAME EMAIL=$EMAIL passphrase2pgp -e -s -a > prv.asc
 gpg --import prv.asc
 rm prv.asc
 #passphrase2pgp -e -s -a -p > pub.asc
@@ -17,7 +19,8 @@ rm prv.asc
 #cat pub.asc | qrencode -o pub.png
 #rm prv.asc pub.asc
 
-echo "do: gpg --edit-key #choose trust->5->quit"
+echo "********** do: gpg --edit-key #choose trust->5->quit"
+gpg --edit-key van
 
 gpg -K --keyid-format long
 
@@ -25,8 +28,8 @@ pkill -HUP gpg-agent
 
 mkdir -p ~/.ssh
 chmod 700 ~/.ssh
-passphrase2pgp -e1 -s -a -f ssh > ~/.ssh/id_rsa
-passphrase2pgp -e1 -s -a -p -f ssh > ~/.ssh/id_rsa.pub
+REALNAME=$REALNAME EMAIL=$EMAIL passphrase2pgp -e1 -s -a -f ssh > ~/.ssh/id_rsa
+REALNAME=$REALNAME EMAIL=$EMAIL passphrase2pgp -e1 -s -a -p -f ssh > ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id*
 
 pkill -HUP ssh-agent
